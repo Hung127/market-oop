bea7bed2c82a32f69541852ea684b4a4e8a2855e
đsea <24120301@student.hcmus.edu.vn>
2025-12-03 23:44:40 +0700

    Refactor UserFactory to use DTOs and expected type
    Updated UserFactory to use UserDto and added expected return type.

--- include/UserFactory.h
+++ include/UserFactory.h
@@ -1,21 +1,37 @@
 #ifndef USER_FACTORY_H
 #define USER_FACTORY_H
 
-#include <expected>
 #include <memory>
+#include <string>
+#include <expected>
 
-#include "Buyer.h"
-#include "Seller.h"
-#include "User.h"
+#include "User_DTO.h"
+#include "Buyer_DTO.h" 
+#include "Seller_DTO.h"
 
-enum class UserRole { BUYER, SELLER };
-
-class UserFactory {
-   public:
-    static std::expected<std::unique_ptr<User>, std::string>
-    createUser(UserRole role, const std::string& id, const std::string& name,
-               const std::string& email, const std::string& password,
-               double balance = 0.0);  // Optional params cho Buyer
+enum class UserRole { 
+    BUYER, 
+    SELLER 
 };
 
-#endif  // USER_FACTORY_H
+class UserFactory {
+public:
+    static std::expected<std::unique_ptr<UserDto>, std::string>
+    createUser(UserRole role, const std::string& id, const std::string& name,
+               const std::string& email, const std::string& password,
+               double balance = 0.0) {
+
+        if (role == UserRole::BUYER) {
+            return std::make_unique<BuyerDto>(id, name, email, password, balance);
+        }
+
+        if (role == UserRole::SELLER) {
+           
+            return std::make_unique<SellerDto>(id, name, email, password);
+        }
+
+        return std::unexpected("Role khong hop le!");
+    }
+};
+
+#endif // USER_FACTORY_H
